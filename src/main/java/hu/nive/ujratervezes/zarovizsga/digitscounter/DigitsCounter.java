2dbc0fecbcf2b8245ec973d74b4bb6644714b9ba
kovacseni <kovacseni@gmail.com>
2021-03-16 19:41:55 +0100

    zarovizsga_DigitsCounter_javitas

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -11,10 +11,15 @@ public class DigitsCounter {
         }
         char[] charsOfStr = str.toCharArray();
         String digits = "0123456789";
+
+        return getCount(charsOfStr, digits);
+    }
+
+    private int getCount(char[] charsOfStr, String digits) {
         List<Character> result = new ArrayList<>();
 
         for (char c : charsOfStr) {
-            if (digits.indexOf(c) >= 0 && result.indexOf(c) < 0) {
+            if (digits.indexOf(c) >= 0 && !result.contains(c)) {
                 result.add(c);
             }
         }
